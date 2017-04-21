cff5fd100e700f956f8696db75be430337133161
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-04-21 21:56:58 +0900

    [ADD] Implement unit tests

---
+++ library/src/test/java/com/lakeel/altla/android/property/AbstractPropertyTest.java
@@ -0,0 +1,65 @@
+package com.lakeel.altla.android.property;
+
+import org.junit.Test;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public final class AbstractPropertyTest {
+
+    @Test
+    public void addOnValueChangedListener() {
+        final AbstractProperty property = new AbstractProperty() {
+            @Nullable
+            @Override
+            public Object getValue() {
+                return null;
+            }
+
+            @Override
+            public void setValue(@Nullable Object value) {
+            }
+        };
+
+        final MockOnValueChangedListener listener = new MockOnValueChangedListener();
+        property.addOnValueChangedListener(listener);
+        property.raiseOnValueChanged();
+
+        assertTrue(listener.invoked);
+    }
+
+    @Test
+    public void removeOnValueChangedListener() {
+        final AbstractProperty property = new AbstractProperty() {
+            @Nullable
+            @Override
+            public Object getValue() {
+                return null;
+            }
+
+            @Override
+            public void setValue(@Nullable Object value) {
+            }
+        };
+
+        final MockOnValueChangedListener listener = new MockOnValueChangedListener();
+        property.addOnValueChangedListener(listener);
+        property.removeOnValueChangedListener(listener);
+        property.raiseOnValueChanged();
+
+        assertFalse(listener.invoked);
+    }
+
+    private final class MockOnValueChangedListener implements Property.OnValueChangedListener {
+
+        private boolean invoked;
+
+        @Override
+        public void onValueChanged(@NonNull Property sender) {
+            invoked = true;
+        }
+    }
+}
